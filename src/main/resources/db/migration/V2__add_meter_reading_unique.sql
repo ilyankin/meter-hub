dc616fc7c449f99164b7fbd0523f64ccd201b2af
ilyankin <lomernap881@gmail.com>
2026-06-09 17:09:44 +0300

    fix: remove comment on constraint for h2 compatibility

--- src/main/resources/db/migration/V2__add_meter_reading_unique.sql
+++ src/main/resources/db/migration/V2__add_meter_reading_unique.sql
@@ -1,5 +1,3 @@
 -- Одно показание на прибор и дату: страховка БД за прикладной проверкой дублей при импорте CSV
 ALTER TABLE meter_readings
     ADD CONSTRAINT uq_meter_readings_meter_date UNIQUE (meter_id, reading_date);
-
-COMMENT ON CONSTRAINT uq_meter_readings_meter_date IS 'Одно показание на прибор и дату';
